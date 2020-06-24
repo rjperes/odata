c8f2d5afd2d90f595d2f93758450f6a09ae74503
Ricardo Peres <rjperes@hotmail.com>
2020-06-24 18:21:09 +0100

    Fixed path

--- odata.server/Program.cs
+++ odata.server/Program.cs
@@ -1,26 +1,39 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace odata.server
 {
-    public class Program
+    public static class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args) =>
+            CreateWebHostBuilder(args)
+                .Build()
+                .CreateDbIfNotExists()
+                .Run();
+
+        private static IHost CreateDbIfNotExists(this IHost host)
         {
-            CreateHostBuilder(args).Build().Run();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var parentChildContext = services.GetRequiredService<ParentChildContext>();
+                parentChildContext.Database.EnsureCreated();
+
+                var blogContext = services.GetRequiredService<BlogContext>();
+                blogContext.Database.EnsureCreated();
+            }
+
+            return host;
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host
+                .CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(builder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    builder.UseStartup<Startup>();
                 });
     }
 }
--- odata.server/Startup.cs
+++ odata.server/Startup.cs
@@ -1,24 +1,49 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OData.Edm;
+using odata.common;
+using System;
+using System.Linq;
 
 namespace odata.server
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
+            this.Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ParentChildContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddDbContext<BlogContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddOData();
+            services.AddODataQueryFilter(new PagingValidatorQueryAttribute());
+
+            services.
+                AddControllers()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -27,14 +52,97 @@ namespace odata.server
             }
 
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapODataRoute("odata", "odata", GetEdmModel(app.ApplicationServices));
+                endpoints.Select().Expand().OrderBy().Filter().Count().MaxTop(10);
+                endpoints.EnableDependencyInjection();
             });
+        }
+
+        private static IEdmModel GetEdmModel(IServiceProvider serviceProvider)
+        {
+            var builder = new ODataConventionModelBuilder(serviceProvider);
+
+            builder
+                .EntitySet<Parent>("Parents")
+                .EntityType
+                .Select()
+                .Expand()
+                .OrderBy()
+                .Filter()
+                .Count();
+
+            builder
+                .EntitySet<Child>("Children")
+                .EntityType
+                .Select()
+                .Expand()
+                .OrderBy()
+                .Filter()
+                .Count();
+
+            builder
+                .EntitySet<Blog>("Blogs")
+                .EntityType
+                .Select()
+                .Expand()
+                .OrderBy()
+                .Filter()
+                .Count();
+
+            builder
+                .EntitySet<Post>("Posts")
+                .EntityType
+                .Select()
+                .Expand()
+                .OrderBy()
+                .Filter()
+                .Count();
+
+            var findByCreation = builder
+                .EntitySet<Blog>("Blogs")
+                .EntityType
+                .Collection
+                .Function("FindByCreation");
+
+            findByCreation.ReturnsCollectionFromEntitySet<Blog>("Blogs");
+            findByCreation.Parameter<DateTime>("date").Required();
+
+
+            var countPosts = builder
+                .EntitySet<Blog>("Blogs")
+                .EntityType
+                .Collection
+                .Function("CountPosts");
+
+            countPosts.Parameter<int>("id").Required();
+            countPosts.Returns<int>();
+
+
+            var count = builder
+                .EntitySet<Blog>("Blogs")
+                .EntityType
+                .Action("Count");
+
+            count.Parameter<int>("id").Required();
+            count.Returns<int>();
+
+
+            var update = builder
+                .EntitySet<Blog>("Blogs")
+                .EntityType
+                .Action("Update");
+
+            update.EntityParameter<Blog>("blog").Required();
+            update.ReturnsFromEntitySet<Blog>("Blogs");
+
+
+            builder
+                .Function("CountBlogPosts")
+                .Returns<int>();
+
+            return builder.GetEdmModel();
         }
     }
 }
